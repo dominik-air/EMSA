0f09f58d84bf6aa18a21c055cc8491a8a3053c97
dominik-air <domzurek@student.agh.edu.pl>
2023-11-30 20:58:57 +0100

    feat: add new group popup

--- emsa-ui/src/components/GroupList.tsx
+++ emsa-ui/src/components/GroupList.tsx
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   List,
   ListItem,
   Button,
   Typography,
   ListItemButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField
 } from "@mui/material";
 
 const GroupList: React.FC = () => {
+  const [open, setOpen] = useState(false);
   const groups = ["group 1", "group 2", "group 3", "group 4", "group 5"];
 
   const handleAddNewGroup = () => {
-    console.log(`Opening add new group pop up!`);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   const handleGroupClick = (group: string) => {
     console.log(`Redirecting to the ${group}'s Homepage!`);
   };
-
   return (
+    <>
     <List sx={{ width: "100%", bgcolor: "background.paper" }}>
       <ListItem>
         <Typography
@@ -52,6 +62,25 @@ const GroupList: React.FC = () => {
         </ListItem>
       ))}
     </List>
+     <Dialog open={open} onClose={handleClose}>
+     <DialogTitle>Create new group</DialogTitle>
+     <DialogContent>
+       <TextField
+         autoFocus
+         margin="dense"
+         id="group-name"
+         label="Group Name"
+         type="text"
+         fullWidth
+         variant="standard"
+       />
+     </DialogContent>
+     <DialogActions>
+       <Button variant="contained" onClick={handleClose}>Cancel</Button>
+       <Button variant="contained" onClick={handleClose}>Create</Button>
+     </DialogActions>
+   </Dialog>
+   </>
   );
 };
 
